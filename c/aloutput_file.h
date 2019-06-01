aba5a3f20f02489e9bd197428ac5e64066c79fdb
philippe lhardy <pl@artisanlogiciel.net>
2019-06-01 11:32:45 +0200

    fix missing aloutput_file.h
    - add it

---
+++ c/aloutput_file.h
@@ -0,0 +1,8 @@
+#ifndef ALOUTPUT_FILE_HEADER_
+#define ALOUTPUT_FILE_HEADER_
+
+#include "aloutput.h"
+
+enum al_global_error_code  aloutput_file_open_init(struct aloutputstream * output, const char * filename);
+
+#endif
