7f74836e7199414ffd55783d7045208014a625f0
philippe lhardy <pl@artisanlogiciel.net>
2019-06-01 18:34:27 +0200

    create a out.dot for dot and dotty
    cd c/tests
    rm out.dot
    ../../build/hashtree 'ceci est la premiere pharse' 'la seconde phrase' 'la troisieme phrase' '4' '5' '6' 'sept' 'huit' 'neuf' '10' '.' '.' '.' '.' '.' 'a' 'b' 'c'
    dot out.dot >out2.dot
    dotty out2.dot

--- c/alhashtree_test.c
+++ c/alhashtree_test.c
@@ -10,15 +10,71 @@ void usage()
 {
   printf("Don't use, even at your own risks\n");
   printf("");
-  printf("add each argument at new rightmost element of btree with hash256 value then dump it.\n");
-    
+  printf("add each argument at new rightmost element of btree with hash256 value then dump it.\n"); 
 }
+
+struct aloutputstream * dotoutput;
+
+void  alhashtree_to_dot(struct aloutputstream * output, struct alhashtreenode * treenode)
+{
+  struct albtree * btreenode = &treenode->btree;
+  if ( btreenode->left != NULL )
+    {
+      if ( btreenode->right != NULL )
+	{
+	  aloutputstream_printf_1k(output,
+				   "node%p -> node%p [color=blue];\n"
+				   "node%p -> node%p [color=red];\n",
+				   btreenode,
+				   btreenode->left,
+				   btreenode,
+				   btreenode->right);
+	}
+      else
+	{
+	  aloutputstream_printf_1k(output,
+				   "node%p -> node%p [color=blue];\n",
+				   btreenode,
+				   btreenode->left);
+	}
+    }
+  else
+    {
+      if ( btreenode->right != NULL )
+	{
+	  aloutputstream_printf_1k(output,
+				   "node%p -> node%p [color=red];\n",
+				   btreenode,
+				   btreenode->right);
+	}
+      else
+	{
+	  if ( btreenode->data != NULL )
+	    {
+	      aloutputstream_printf_1k(output,"node%p [label=\"%s\",color=blue];\n",btreenode,(char *) btreenode->data);
+	    }
+	  else
+	    {
+	      aloutputstream_printf_1k(output,"node%p [color=red];\n",btreenode);
+	    }
+
+	}
+
+    }
+
+  
+}
+
 
 void alhashtree_data_process(void * data, void * contextdata, struct albtree * btree)
 {
   // to improve
   struct alhashtreenode * treenode = (struct alhashtreenode *) btree;
   alhashtree_dump_treenode(NULL,treenode);
+  if ( dotoutput != NULL )
+    {
+      alhashtree_to_dot(dotoutput,treenode);
+    }
 }
   
 int main(int argc, char ** argv)
@@ -42,6 +98,10 @@ int main(int argc, char ** argv)
   struct aloutputstream out;
   aloutput_file_open_init(&out,"out.dbg");
   alhashtree_global_init(&out,&context);
+
+  struct aloutputstream dotout;
+  aloutput_file_open_init(&dotout,"out.dot");
+  dotoutput = &dotout;
 
   treenode = alhashtree_create(&context);
 
@@ -68,10 +128,21 @@ int main(int argc, char ** argv)
 
   aldebug_printf(NULL,"depth %i\n",depth);
 
+  if ( dotoutput != NULL )
+    {
+      aloutputstream_printf_1k(dotoutput,"digraph root%p {\n", &root->btree);
+    }
+
   albtree_walk(&root->btree, ALBTREE_WP_SLR,  alhashtree_data_process, NULL, 10);
   alhashtree_clean(treenode);
 
   alstrings_ringbuffer_release(&context.ringbuffer);
+
+  
+  if ( dotoutput != NULL )
+    {
+      aloutputstream_printf_1k(dotoutput,"}\n", &root->btree);
+    }
 
   return 0;  
 }
