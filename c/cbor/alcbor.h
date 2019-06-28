0498c38357f2a106afef3adaa031950e21762d2b
philippe lhardy <pl@artisanlogiciel.net>
2019-06-28 21:22:21 +0200

    minor move cbor constants into cbor.h
    - il fait vraiment chaud avec la canicule

---
+++ c/cbor/alcbor.h
@@ -0,0 +1,25 @@
+#ifndef ALCBOR_HEADER_
+#define ALCBOR_HEADER_
+
+
+// field encoding type
+typedef enum alcbor_field_encoding_type {
+					 ALCBOR_FET_NOT_SET = -2,
+					 ALCBOR_FET_NYI = -2,
+					 ALCBOR_FET_ERROR = -1,
+					 ALCBOR_FET_TINY = 0,
+					 ALCBOR_FET_SHORT = 1,
+					 ALCBOR_FER_LONG = 2
+} alcbor_fet;
+
+typedef enum alcbor_major_type {
+				ALCBOR_MT0_UINT = 0,
+				ALCBOR_MT1_NINT = 1,
+				ALCBOR_MT2_BSTR = 2,
+				ALCBOR_MT4_ARRAY = 4,
+} alcbor_mt;
+
+
+
+#endif // ALCBOR_HEADER_
+ 
--- c/cbor/alcbor_decoder.h
+++ c/cbor/alcbor_decoder.h
@@ -1,27 +1,11 @@
 #ifndef ALCBOR_DECODER__
 #define ALCBOR_DECODER__
 
+#include "alcbor.h"
+
 #include "alinput.h"
 #include "aloutput.h"
 #include "aljson.h"
-
-// field encoding type
-typedef enum alcbor_field_encoding_type {
-					 ALCBOR_FET_NOT_SET = -2,
-					 ALCBOR_FET_NYI = -2,
-					 ALCBOR_FET_ERROR = -1,
-					 ALCBOR_FET_TINY = 0,
-					 ALCBOR_FET_SHORT = 1,
-					 ALCBOR_FER_LONG = 2
-} alcbor_fet;
-
-typedef enum alcbor_major_type {
-				ALCBOR_MT0_UINT = 0,
-				ALCBOR_MT1_NINT = 1,
-				ALCBOR_MT2_BSTR = 2,
-				ALCBOR_MT4_ARRAY = 4,
-} alcbor_mt;
-
 
 struct alcbor_json_output {
   struct json_parser_ctx json_ctx;
@@ -43,7 +27,6 @@ typedef struct alcbor_parsing_context {
 } alcbor_pc;
 
 #endif // ALCBOR_DECODER__
-
 
 void alcbor_parsing_context_init(alcbor_pc * context, struct alinputstream * input, struct aloutputstream * output);
 
