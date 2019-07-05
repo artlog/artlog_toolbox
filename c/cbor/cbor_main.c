b98db23074b3d2df71bd5cd8672d0e46ecc9c71e
philippe lhardy <pl@artisanlogiciel.net>
2019-07-05 16:54:40 +0200

    cbor encoding add usage
    - update usage to clarify usage
    - bump to version 0.2 (incredible no ?)

--- c/cbor/cbor_main.c
+++ c/cbor/cbor_main.c
@@ -10,17 +10,18 @@
 
 #include <stddef.h>
 
-const char * CBOR_MAIN_VERSION = "0.1";
+const char * CBOR_MAIN_VERSION = "0.2";
 
 void usage()
 {
-  aldebug_printf(DBGSTREAM,"version %s\nconvert cbor to json. Not fully implemented\n", CBOR_MAIN_VERSION);
+  aldebug_printf(DBGSTREAM,"version %s\nconvert cbor <-> json  Not fully implemented\n", CBOR_MAIN_VERSION);
   aldebug_printf(DBGSTREAM,"https://tools.ietf.org/html/rfc7049\n");
   aldebug_printf(DBGSTREAM,"https://en.wikipedia.org/wiki/CBOR\n");
 
   aldebug_printf(DBGSTREAM,"hexstring=<hex string> to be converted to raw bytes in outfile ( in this case cbor)\n");
-  aldebug_printf(DBGSTREAM,"infile=<cbor input file>\n");
-  aldebug_printf(DBGSTREAM,"outfile=<json output file to be created>\n");
+  aldebug_printf(DBGSTREAM,"inform=... if set infile is read as json\n");
+  aldebug_printf(DBGSTREAM,"infile=<cbor or json input file>\n");
+  aldebug_printf(DBGSTREAM,"outfile=<json or cbor output file to be created>\n");
 }
 
 // CBOR encoded data is seen as a stream of data items
