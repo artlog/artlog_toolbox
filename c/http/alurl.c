60800ea7658c9d95382e17cb60eeb8854bbb5214
philippe lhardy <pl@artisanlogiciel.net>
2023-02-10 21:28:10 +0100

    garbage code collecting
    - old code found on another laptop
      not even sure it was used, still shows parsing was done 'manualy'

--- c/http/alurl.c
+++ c/http/alurl.c
@@ -18,8 +18,75 @@ int alurl_context_init(struct alurl_context * context, void * ext, int ext_hint)
   return 0;
 }
 
+int eat_DIGIT(struct alurl_context * context, char start, char end)
+{
+  int v = -1;
+  // 0-9
+  if ( c >= start && c <= '9' )
+    {
+      v = c - '0';
+    }
+  return v;
+}
+
+int eat_dec_octet(struct alurl_context * context)
+{
+  // 0-9
+  eat_DIGIT(context,'0','9');
+  // 10-99
+  eat_DIGIT(context,'1','9');
+  // 100-199
+  eat_char('1');
+  eat_DIGIT(context,'0','9');
+  eat_DIGIT(context,'0','9');
+  // 200-249
+  // 250-255
+  eat_string(25);
+  eat_DIGIT(context,'0','5');
+}
+  
+int eat_IPV4address(struct alurl_context * context)
+{
+  eat_decoctet(context);
+  eat_char('.');
+  eat_decoctet(context);
+  eat_char('.');
+  eat_decoctet(context);
+  eat_char('.');
+  eat_decoctet(context);
+}
+
+int eat_ihost(struct alurl_context * context)
+{
+  // IP-literal
+  // IPV4address
+  // ireg-name
+}
+
+
 struct alurl * alurl_parse_new(struct alurl_context * context, char * url)
 {
+  if (eat_string("http"))
+    {
+      if (eat_char('s'))
+	{
+	  context->set_scheme(HTTPS);
+	}
+      else
+	{
+	  context->set_scheme(HTTP);
+	}
+      if (eat_char(':'))
+	{
+	  eat_string("//");
+	  eat_ihost();
+	  if (eat_char(':'))
+	    {
+	      context->set_port(eat_int());
+	    }
+	}
+
+    }
   return NULL;
 }
 
